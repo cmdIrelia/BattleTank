3a47d6b53578cd659b7a5245ff1017cb377d88aa
Irelia <30620832+cmdIrelia@users.noreply.github.com>
2019-01-05 22:38:18 +0800

    Using virtual and override

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,7 +3,20 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();	//执行父类的BeginPlay函数
 
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank Player Controller NOT acquried."));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank Player Controller acquried: %s"), *(ControlledTank->GetName()));
+	}
+}
 
 ATank *ATankPlayerController::GetControlledTank() const
 {
